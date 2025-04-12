6e63ee7b5256f2a2ca469c45009409ddab5e983a
Nico235711 <139637937+Nico235711@users.noreply.github.com>
2025-04-12 14:36:23 -0300

    eliminando

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,33 @@
-import { Guitar } from "./components/Guitar"
-import { Header } from "./components/Header"
-import { useCart } from "./hooks/useCart"
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-const App = () => {
-
-  const {
-    data,
-    cart,
-    addToCart,
-    removeFromCart,
-    increaseQuantity,
-    decreaseQuantity,
-    cleanCart,
-    isEmpty,
-    total
-  } = useCart()
+function App() {
+  const [count, setCount] = useState(0)
 
   return (
     <>
-      <Header
-        cart={cart}
-        removeFromCart={removeFromCart}
-        increaseQuantity={increaseQuantity}
-        decreaseQuantity={decreaseQuantity}
-        cleanCart={cleanCart}
-        isEmpty={isEmpty}
-        total={total}
-      />
-
-      <main className="container-xl mt-5">
-        <h2 className="text-center">Nuestra Colección</h2>
-
-        <div className="row mt-5">
-          {data.map(guitar => (
-            <Guitar
-              key={guitar.id}
-              guitar={guitar}
-              addToCart={addToCart}
-            />
-          ))}
-        </div>
-      </main>
-
-      <footer className="bg-dark mt-5 py-5">
-        <div className="container-xl">
-          <p className="text-white text-center fs-4 mt-4 m-md-0">
-            GuitarLA - Todos los derechos Reservados
-          </p>
-        </div>
-      </footer>
-
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
   )
 }
